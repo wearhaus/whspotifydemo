82f5709f53e49fc925eac449c58bcac36a423c33
Kenneth Lauguico <ken@wearhaus.com>
2016-02-19 11:43:20 -0800

    Initial Commit

---
+++ WHSpotifyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHSpotifyDemo
+//
+//  Created by Ken Lauguico on 2/19/16.
+//  Copyright © 2016 Ken Lauguico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
